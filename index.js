98d2b0a176df775e4ddc65ca22940a18202d9cd6
Anna Harutyunyan <anuanharutyunyan>
2022-01-30 21:17:26 +0400

    feat(winner):DoesCartWinPrize

--- index.js
+++ index.js
@@ -3,33 +3,15 @@ const WINNING_SUM = 123.4;
 /**
 @param {Array} cartItemsPrices
 */
-function isIntegar(element) {
-  return element % 1 !== 0;
-}
 
 function doesCartWinPrize(cartItemsPrices) {
-  if (cartItemsPrices.every(isIntegar)) {
-    let sum = cartItemsPrices.reduce((a, b) => a + b, 0);
-    const fixedNum = parseFloat(sum).toFixed(2);
-    return fixedNum == WINNING_SUM;
+  let sum = 0;
+
+  for (let i = 0; i < cartItemsPrices.length; i++) {
+    sum += +cartItemsPrices[i];
   }
-  if (
-    cartItemsPrices.every(function (element) {
-      return typeof element === "number";
-    })
-  ) {
-    const sum = cartItemsPrices.reduce((a, b) => a + b, 0);
-    return sum === WINNING_SUM;
-  }
-  if (
-    cartItemsPrices.every(function (element) {
-      return typeof element === "string";
-    })
-  ) {
-    cartItemsPrices = cartItemsPrices.map(Number);
-    const sum = cartItemsPrices.reduce((a, b) => a + b, 0);
-    return sum === WINNING_SUM;
-  }
+
+  return +sum.toFixed(2) === WINNING_SUM;
 }
 
 module.exports = doesCartWinPrize;
